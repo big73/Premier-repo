2aca3f812b3cf303b39412e412fe31bc87e1cdb6
big73 <big73003@gmail.com>
2015-12-01 14:34:11 +0100

    Mise en place de la classe de control.

---
+++ Calculatrice/src/fr/actia/control/Control.java
@@ -0,0 +1,13 @@
+package fr.actia.control;
+
+import fr.actia.entities.Division;
+
+public class Control {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Division div = new Division();
+
+	}
+
+}
